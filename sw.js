52ebf17a2baee33c4cf9a275bad93156ed8f1193
brianmartinezroz-alt <brianmartinezroz@gmail.com>
2026-09-19 22:12:51 -0600

    Icono azul y tipografia Fraunces / DM Sans / DM Mono (familia EFA)

--- sw.js
+++ sw.js
@@ -2,7 +2,7 @@
    Guarda la app en el teléfono para que abra sin internet.
    Sube el número de CACHE cada vez que publiques una versión nueva. */
 
-var CACHE = 'dinero-v6';
+var CACHE = 'dinero-v7';
 var ARCHIVOS = ['./', './index.html', './manifest.json', './icon.png'];
 
 self.addEventListener('install', function (e) {
